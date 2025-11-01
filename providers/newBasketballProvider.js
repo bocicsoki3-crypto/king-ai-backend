3d7d3f27d45200295fc07268daddaecf5e44ee63
bocicsoki3-crypto <bocicsoki3@gmail.com>
2025-11-01 08:33:34 +0100

    Update newBasketballProvider.js

--- providers/newBasketballProvider.js
+++ providers/newBasketballProvider.js
@@ -1,24 +1,26 @@
 // providers/newBasketballProvider.js
 import axios from 'axios';
-import { makeRequest } from './common/utils.js'; // Használjuk a közös request hívót
+import { makeRequest } from './common/utils.js';
+// Használjuk a közös request hívót
 
-// Olvassuk be az ÚJ kulcsot a .env fájlból
-const { BASKETBALL_API_KEY } = process.env;
-const BASKETBALL_API_HOST = 'basketball-api.p.rapidapi.com'; // Példa API
+// JAVÍTÁS: Importálás a config.js-ből
+import {
+    BASKETBALL_API_KEY,
+    BASKETBALL_API_HOST
+} from '../config.js';
 
 /**
  * 🏀 Kosárlabda Adatlekérő Függvény
  */
 export async function fetchMatchData(options) {
   const { sport, homeTeamName, awayTeamName, leagueName, utcKickoff } = options;
-
+// JAVÍTÁS: Importált konstansok használata
   if (!BASKETBALL_API_KEY) {
-    throw new Error('[Basketball API] Hiányzó BASKETBALL_API_KEY a .env fájlban.');
-  }
+    throw new Error('[Basketball API] Hiányzó BASKETBALL_API_KEY a config.js-ben.');
+}
   
   console.log(`[Basketball Provider]: Adatgyűjtés indul: ${homeTeamName} vs ${awayTeamName}`);
-
-  // 1. API HÍVÁSOK
+// 1. API HÍVÁSOK
   // TODO: Implementáld a kosárlabda API hívásaidat
   
   // 2. GEMINI HÍVÁS (opcionális)
@@ -26,22 +28,20 @@ export async function fetchMatchData(options) {
   // 3. ADAT EGYSÉGESÍTÉS (NORMALIZÁLÁS)
   // KRITIKUS LÉPÉS: Az adatokat át kell alakítanod UGYANARRA
   // a 'result' struktúrára, mint a többi provider!
-  
-  const unifiedResult = {
-    rawStats: { home: { gp: 0 }, away: { gp: 0 } }, // TODO: Töltsd fel valós adatokkal
+const unifiedResult = {
+    rawStats: { home: { gp: 1 }, away: { gp: 1 } }, // JAVÍTÁS: GP 1-re állítva a hiba elkerülése végett
     leagueAverages: {},
-    richContext: "Kosárlabda specifikus kontextus...", // TODO
+    richContext: "Kosárlabda specifikus kontextus (Placeholder)...", // TODO
     advancedData: { home: {}, away: {} }, // TODO
     form: { home_overall: "N/A", away_overall: "N/A" }, // TODO
     rawData: { /* ... a nyers API válaszok ... */ },
     oddsData: null, // TODO
     fromCache: false
   };
-
-  // Ellenőrzés
+// Ellenőrzés
   if (unifiedResult.rawStats.home.gp <= 0) {
      throw new Error(`Kritikus statisztikák (GP <= 0) érvénytelenek (Basketball).`);
-  }
+}
 
   return unifiedResult;
 }
